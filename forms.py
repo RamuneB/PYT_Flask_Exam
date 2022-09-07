e4a6edc2c236c7d9663f8b33aecaded01de986c3
Ramunė Butauskienė <101128898+RamuneB@users.noreply.github.com>
2022-09-07 21:34:00 +0300

    add form

---
+++ forms.py
@@ -0,0 +1,57 @@
+from flask_wtf import FlaskForm
+from wtforms import SubmitField, BooleanField, StringField, PasswordField, TextAreaField, IntegerField
+from wtforms.validators import DataRequired, ValidationError, EqualTo
+from wtforms_sqlalchemy.fields import QuerySelectField
+from flask_wtf.file import FileField, FileAllowed
+import app
+
+
+class RegistracijosForma(FlaskForm):
+    vardas = StringField('Vardas', [DataRequired()])
+    el_pastas = StringField('El. paštas', [DataRequired()])
+    slaptazodis = PasswordField('Slaptažodis', [DataRequired()])
+    patvirtintas_slaptazodis = PasswordField("Pakartokite slaptažodį", [EqualTo('slaptazodis', "Slaptažodis turi sutapti.")])
+    submit = SubmitField('Prisiregistruoti')
+
+
+class PrisijungimoForma(FlaskForm):
+    el_pastas = StringField('El. paštas', [DataRequired()])
+    slaptazodis = PasswordField('Slaptažodis', [DataRequired()])
+    prisiminti = BooleanField("Prisiminti mane")
+    submit = SubmitField('Prisijungti')
+
+
+class StraipsnioForma(FlaskForm):
+    pavadinimas = StringField('Pavadinimas', [DataRequired()])
+    tekstas = TextAreaField('Tekstas', [DataRequired()])
+    grupe = QuerySelectField(
+        query_factory=lambda: app.Grupe.query, 
+        get_label="pavadinimas", 
+        get_pk=lambda obj: obj.id,
+        allow_blank=True)
+    submit = SubmitField('Išsaugoti')
+
+
+class PaskyrosAtnaujinimoForma(FlaskForm):
+    vardas = StringField('Vardas', [DataRequired()])
+    el_pastas = StringField('El. paštas', [DataRequired()])
+    nuotrauka = FileField('Atnaujinti profilio nuotrauką', validators=[FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Atnaujinti')
+
+class IncomeForm(FlaskForm):
+    suma = IntegerField('Suma', [DataRequired()])
+    #siuntejas = StringField('Siuntėjas', [DataRequired(message='Nenurodytas siuntėjas')])
+    papildoma_info = TextAreaField('Papildoma info')
+    submit = SubmitField('Išsaugoti')
+
+    def tikrinti_varda(self, vardas):
+        if vardas.data != app.current_user.vardas:
+            vartotojas = app.Vartotojas.query.filter_by(vardas=vardas.data).first()
+            if vartotojas:
+                raise ValidationError('Šis vardas panaudotas. Pasirinkite kitą.')
+
+    def tikrinti_pasta(self, el_pastas):
+        if el_pastas.data != app.current_user.el_pastas:
+            vartotojas = app.Vartotojas.query.filter_by(el_pastas=el_pastas.data).first()
+            if vartotojas:
+                raise ValidationError('Šis el. pašto adresas panaudotas. Pasirinkite kitą.')
